8735143bbb0cdae4f8c8ea9c1659e1c1b126052a
Prathamesh Mali <mali.prathamesh82@gmail.com>
2020-12-16 12:44:59 +0530

    Connect app to remote mongoDB cluster

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const app = require('./app')
+const mongoose = require('mongoose')
 require('dotenv').config()
 
 console.log(app.get('env'))
+
+// Connect to mongodb
+const DB = process.env.DATABASE_URI
+const localDB = process.env.DATABASE_LOCAL
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(res => console.log('Remote Mongo DB connected ✅'))
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`))

7ade87974244a031a5f2c2469b84a559ccf21fb8
Prathamesh Mali <mali.prathamesh82@gmail.com>
2020-12-23 14:44:48 +0530

    installed jwt and completed signup with jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,25 @@
 const catchAsync = require('../utils/catchAsync')
 const User = require('./../models/userModel')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
 exports.signUp = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body)
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    confirmedPassword: req.body.confirmedPassword,
+  })
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  })
+
   res.status(201).json({
-    status: 'Successfull',
+    status: 'Successful',
+    token,
     data: {
-      newUser,
+      user: newUser,
     },
   })
 })

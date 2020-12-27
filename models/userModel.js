6fd8a092f32d51c314efd4e45c9c4232e4557580
Prathamesh Mali <mali.prathamesh82@gmail.com>
2020-12-27 14:55:16 +0530

    Added middleware for filtering inactive users

--- models/userModel.js
+++ models/userModel.js
@@ -41,6 +41,11 @@ const userSchema = new mongoose.Schema({
   passwordChangedAt: Date,
   passwordResetToken: String,
   passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
 })
 
 // Encryption using the preSave mongo middleware
@@ -65,6 +70,13 @@ userSchema.pre('save', function (next) {
 
   // If user is modified add the property
   this.passwordChangedAt = Date.now() - 1000
+  next()
+})
+
+// Dont send inactive users
+userSchema.pre(/^find/, function (next) {
+  //This refers to the current query
+  this.find({ active: { $ne: false } })
   next()
 })
 

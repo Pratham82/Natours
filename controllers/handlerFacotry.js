b69fb98b54670a10a352080b2dbc928cd42690f2
pratham82 <mali.prathamesh82@gmail.com>
2021-01-04 20:01:23 +0530

    Added new handler for creating documents

--- controllers/handlerFacotry.js
+++ controllers/handlerFacotry.js
@@ -14,3 +14,35 @@ exports.deleteOne = Model =>
       data: 'Document Successfully deleted',
     })
   })
+
+exports.updateOne = Model =>
+  catchAsync(async (req, res, next) => {
+    const updatedDoc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    })
+
+    if (!updatedDoc) {
+      return next(new AppError('No document found with this ID', 404))
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        updatedDoc,
+      },
+    })
+  })
+
+exports.createOne = Model =>
+  catchAsync(async (req, res, next) => {
+    //const newTour = new Tour({})
+    const document = await Model.create(req.body)
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        document,
+      },
+    })
+  })

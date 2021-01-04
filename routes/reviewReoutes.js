3773be2ec2be63d95c3e6cf3af5d4232b97ef896
pratham82 <mali.prathamesh82@gmail.com>
2021-01-04 21:07:46 +0530

    Added new routes and removed old ones

--- routes/reviewReoutes.js
+++ routes/reviewReoutes.js
@@ -3,12 +3,23 @@ const { protect, restrictTo } = require('./../controllers/authController')
 const {
   getAllReviews,
   createReview,
+  deleteReview,
+  updateReview,
+  getReview,
+  setTourUserIds,
 } = require('./../controllers/reviewController')
 
 const reviewRouter = express.Router({ mergeParams: true }) // This will merge the parameters, so we can access the data coming from the different routers
+
 reviewRouter
   .route('/')
-  .get(protect, restrictTo('user'), getAllReviews)
-  .post(protect, restrictTo('user'), createReview)
+  .get(getAllReviews)
+  .post(protect, restrictTo('user'), setTourUserIds, createReview)
+
+reviewRouter
+  .route('/:id')
+  .delete(protect, restrictTo('user'), deleteReview)
+  .patch(protect, restrictTo('user'), updateReview)
+  .get(protect, restrictTo('user'), getReview)
 
 module.exports = reviewRouter

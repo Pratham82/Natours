74ac38b57c5634b8136bb70fe8c752a60240a8f8
pratham82 <mali.prathamesh82@gmail.com>
2021-01-04 21:07:07 +0530

    Created handler for getOne and getAll documents

--- controllers/handlerFacotry.js
+++ controllers/handlerFacotry.js
@@ -1,5 +1,6 @@
 const catchAsync = require('./../utils/catchAsync')
 const AppError = require('./../utils/appError')
+const APIFeatures = require('./../utils/apiFeatures')
 
 exports.deleteOne = Model =>
   catchAsync(async (req, res, next) => {
@@ -43,6 +44,50 @@ exports.createOne = Model =>
       status: 'success',
       data: {
         document,
+      },
+    })
+  })
+
+exports.getOne = (Model, populateOptions) =>
+  catchAsync(async (req, res, next) => {
+    // In req.params all the parameters are stored
+    // We can use multiple parameters
+    // To make a parameter optional we will have to add '?' after it:
+    // app.get('/api/v0/tours/:id/:x/:y?', (req, res) => {
+    let query = Model.findById(req.params.id)
+    if (populateOptions) query = query.populate(populateOptions)
+    const document = await query
+
+    if (!document) {
+      return next(new AppError('No document found with this ID', 404))
+    }
+
+    res.status(200).send({
+      status: 'success',
+      data: { document },
+    })
+  })
+
+exports.getAll = Model =>
+  catchAsync(async (req, res, next) => {
+    // Get all reviews for specific tour( Nested GET reviews on tour )
+    let filter = {}
+    if (req.params.tourId) filter = { tour: req.params.tourId }
+
+    // Execute query
+    const features = new APIFeatures(Model.find(filter), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate()
+    const documents = await features.query
+
+    res.status(200).send({
+      status: 'success',
+      requestedAt: req.requestTime,
+      results: documents.length,
+      data: {
+        data: documents,
       },
     })
   })

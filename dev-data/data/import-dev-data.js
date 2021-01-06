6f719ea3c65a8d27d72a04fe65eddbbfb8e2cddf
pratham82 <mali.prathamesh82@gmail.com>
2021-01-06 20:52:26 +0530

    Added code for importing project data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const fs = require('fs')
 const Tour = require('../../models/tourModel')
 const User = require('../../models/userModel')
+const Review = require('../../models/reviewModel')
 require('dotenv').config()
 
 // Connect to mongodb
@@ -17,14 +18,18 @@ mongoose
 
 // Read Data
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'))
-
 const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'))
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
+)
 
 //Import Data into DB
 const importData = async () => {
   try {
     // Crating multiple documents by passing array of documents
     await Tour.create(tours)
+    await User.create(users, { validateBeforeSave: false })
+    await Review.create(reviews)
     //    awat User.create(users)
     await console.log('Data Successfully loaded')
     process.exit()
@@ -37,6 +42,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany()
+    await User.deleteMany()
+    await Review.deleteMany()
     console.log('All Data Successfully deleted')
     process.exit()
   } catch (err) {
